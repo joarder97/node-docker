728cac4fad93e13bf4d4141b819aef8bc8ce920a
joarder97 <masudul.pallab99@gmail.com>
2021-11-30 10:25:16 +0600

    Communicating between containers

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
+
+mongoose
+    .connect("mongodb://root:pwd@mongo:27017/?authSource=admin")
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('<h2>Hi there!!<h2>');
